d84bf3698b1e9a524358b52f08cbd8e3627a03bb
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-18 15:37:00 +0200

    Add map function

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./resources.css";
 import List from "../components/List/List";
 import ListItem from "../components/List/ListItem";
 import ListItemText from "../components/List/ListItemText";
 import ListItemIcon from "../components/List/ListItemIcon";
+import { getResources } from "../api/resources";
 
 function Resources() {
+  const [resources, setResources] = useState(null);
+  useEffect(() => {
+    async function getResourceList() {
+      const resources = await getResources();
+      setResources(resources);
+    }
+    getResourceList();
+  }, []);
+
+  console.log(resources);
+
   return (
     <main className="resources">
       <List>
-        <ListItem>
-          <ListItemIcon imgSrc="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png" />
-          <ListItemText primary="Javascript Info" />
-        </ListItem>
+        {resources?.map((resource) => (
+          <ListItem key={resource.id}>
+            <ListItemIcon imgSrc={resource.img} />
+            <ListItemText primary={resource.titel} />
+          </ListItem>
+        ))}
       </List>
     </main>
   );
